f43499ed6dd9175ff446f812383c2af7bc0746f7
nqbao1501 <127926528+nqbao1501@users.noreply.github.com>
2025-08-13 20:43:50 +0700

    Update pid.h

--- Core/Inc/pid.h
+++ Core/Inc/pid.h
@@ -6,9 +6,13 @@ typedef struct {
     float Ki;
     float Kd;
 
-    float setpoint;
-    float prev_error;
-    float integral;
+    float target_value;
+    float measured_value;
+    float measured_value_prev;
+
+    float error;
+    float error_sum;
+    float error_deriv;
 
     float output_limit;     // Max abs(output)
     float integral_limit;   // Max abs(integral term)
@@ -16,8 +20,18 @@ typedef struct {
     float output;           // Last PID output
 }PID_Controller;
 
+typedef struct{
+	PID_Controller outer_loop;
+	PID_Controller inner_loop;
+}Double_PID_Controller;
+
+
 void PID_Init(PID_Controller *pid, float Kp, float Ki, float Kd, float output_limit, float integral_limit);
-float PID_Compute(PID_Controller *pid, float target, float measured, float dt);
+float PID_Compute(PID_Controller *pid, float target, float measured, float measured_rate, float dt);
+float PID_Double_Calculation(Double_PID_Controller *axis, float target_angle, float measured_angle, float measured_rate, float dt);
+float PID_Yaw_Rate_Calculation(PID_Controller *axis, float target_rate, float measured_rate, float dt);
+float PID_Yaw_Angle_Calculation(PID_Controller *axis, float target_angle, float measured_angle, float measured_rate, float dt);
+
 void PID_Reset(PID_Controller *pid);
 
 #endif
